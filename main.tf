7083dda1a05f648d615d594c15d90684793ab0fe
gui-sousa <guira_naruto@hotmail.com>
2022-04-05 11:30:40 -0300

    teste de workflow

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "gcs" {
+    bucket      = "bkt-iis-loadbalancer"
+    prefix      = "dev"
+    #credentials = "gcp-dev.json"
+  }
+}
+
+
+provider "google" {
+  #credentials = file("gcp-dev.json")
+  project     = var.projeto
+  region      = var.regiao
+}
+
+
+
---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "regiao" {
+  default = "us-east1"
+}
+
+variable "projeto" {
+  default = "adfs-343320"
+}
+
+variable "tipo-vm" {
+  default = "n2-highcpu-4"
+}
+
+variable "zona" {
+  default = "us-east1-b"
+}
