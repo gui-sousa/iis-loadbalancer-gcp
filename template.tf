2ac1d4c3f647cfbafc6e25f263402c8fe4d354e2
gui-sousa <guira_naruto@hotmail.com>
2022-05-06 11:34:55 -0300

    feito teste com template do script

--- template.tf
+++ template.tf
@@ -1,6 +1,12 @@
 data "template_file" "script-monitora" {
-    template = "${file("template.go")}"
-    vars = {
-      url_instancia = "${google_compute_instance.instancia-teste.network_interface.0.access_config.0.nat_ip}"
-    }
+  template = file("template.go")
+  vars = {
+    url_instancia = "${google_compute_instance.instancia-teste.network_interface.0.access_config.0.nat_ip}"
+  }
+}
+
+resource "local_file" "monitora" {
+  filename = "./monitora.go"
+  content  = data.template_file.script-monitora.rendered
+
 }
